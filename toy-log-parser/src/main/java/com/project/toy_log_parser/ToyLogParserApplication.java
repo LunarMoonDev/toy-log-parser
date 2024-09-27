1ab775042778063073a9eb32fd8ed3ca6bcb1d21
Harjit Brar <workandresearchbrar@gmail.com>
2024-09-27 16:59:38 +0800

    feat: add spring project boilerplate

---
+++ toy-log-parser/src/main/java/com/project/toy_log_parser/ToyLogParserApplication.java
@@ -0,0 +1,13 @@
+package com.project.toy_log_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyLogParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyLogParserApplication.class, args);
+	}
+
+}
